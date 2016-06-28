74f188206e675be5f0074c5b132c0dccf66a3a4d
Patrick Morris <prmwrites@gmail.com>
2016-06-28 19:37:44 -0400

    Added CrimeFragment model with onCreate methods

---
+++ app/src/main/java/com/grandroid/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,28 @@
+package com.grandroid.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Patrick on 6/28/2016.
+ */
+public class CrimeFragment extends android.support.v4.app.Fragment {
+
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
+    }
+
+}
