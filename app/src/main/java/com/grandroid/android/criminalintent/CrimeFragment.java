22cfac50e7de0512a18aacd3ed61b5fc9b61af67
Patrick Morris <prmwrites@gmail.com>
2016-06-28 19:54:04 -0400

    Wired up textchange listener

--- app/src/main/java/com/grandroid/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/grandroid/android/criminalintent/CrimeFragment.java
@@ -2,9 +2,12 @@ package com.grandroid.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by Patrick on 6/28/2016.
@@ -12,6 +15,7 @@ import android.view.ViewGroup;
 public class CrimeFragment extends android.support.v4.app.Fragment {
 
     private Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,25 @@ public class CrimeFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    // This space left blank
+                }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This one too
+            }
+        });
+
         return v;
     }
 
