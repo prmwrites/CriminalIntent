73514f1a8523e646ed119e502181e2a9a2aa5bcc
Patrick Morris <prmwrites@gmail.com>
2016-06-28 20:57:54 -0400

    Added extra fields and getter/setter to Crime model

--- app/src/main/java/com/grandroid/android/criminalintent/Crime.java
+++ app/src/main/java/com/grandroid/android/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.grandroid.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,10 +9,13 @@ import java.util.UUID;
 public class Crime {
     private UUID mID;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         // Generate unique identifier
         mID = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getID() {
@@ -24,5 +28,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
