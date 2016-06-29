9bad3dfc78211d94f4f940c22d9b62f8b5657098
Patrick Morris <prmwrites@gmail.com>
2016-06-28 22:33:22 -0400

    Created list of crime objects, generated 100 generic crimes

--- app/src/main/java/com/grandroid/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/grandroid/android/criminalintent/CrimeLab.java
@@ -2,11 +2,16 @@ package com.grandroid.android.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by Patrick on 6/28/2016.
  */
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -16,6 +21,25 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); // Every other one
+            mCrimes.add(crime);
+        }
+    }
 
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getID().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
     }
 }
