23f3ec1aaf523a0562ce0bd3a7eb8319724d892b
Patrick Morris <prmwrites@gmail.com>
2016-06-28 21:57:00 -0400

    Wired up remaining widgets, added landscape view

--- app/src/main/java/com/grandroid/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/grandroid/android/criminalintent/CrimeFragment.java
@@ -7,6 +7,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -16,6 +19,8 @@ public class CrimeFragment extends android.support.v4.app.Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,19 @@ public class CrimeFragment extends android.support.v4.app.Fragment {
             @Override
             public void afterTextChanged(Editable s) {
                 // This one too
+            }
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Set the crime's solved property
+                mCrime.setSolved(isChecked);
             }
         });
 
