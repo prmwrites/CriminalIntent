bd38af6b3775dfde5742aced988ea8ef10d59180
Patrick Morris <prmwrites@gmail.com>
2016-07-03 01:28:41 -0400

    CrimPagerActivity class created, ViewPager view created

---
+++ app/src/main/java/com/grandroid/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,15 @@
+package com.grandroid.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Patrick on 7/3/2016.
+ */
+public class CrimePagerActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
+}
