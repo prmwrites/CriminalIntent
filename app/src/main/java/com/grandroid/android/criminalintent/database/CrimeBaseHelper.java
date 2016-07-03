cdfb3d08ccc1d6b76adbaa417d629d2f197db9fa
Patrick Morris <prmwrites@gmail.com>
2016-07-03 12:45:53 -0400

    Created db schema and helper

---
+++ app/src/main/java/com/grandroid/android/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,27 @@
+package com.grandroid.android.criminalintent.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Patrick on 7/3/2016.
+ */
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crimebase.db";
+
+    public CrimeBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
