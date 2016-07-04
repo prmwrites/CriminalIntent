1ce4be41a4cf18e94657a046eb237faba672c135
Patrick Morris <prmwrites@gmail.com>
2016-07-04 13:03:54 -0400

    Created Suspect, added to database classes

--- app/src/main/java/com/grandroid/android/criminalintent/database/CrimeBaseHelper.java
+++ app/src/main/java/com/grandroid/android/criminalintent/database/CrimeBaseHelper.java
@@ -18,7 +18,7 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + CrimeDbSchema.CrimeTable.NAME + "(" + " _id integer primary key autoincrement, " + CrimeDbSchema.CrimeTable.Cols.UUID + ", " + CrimeDbSchema.CrimeTable.Cols.TITLE + ", " +
-                CrimeDbSchema.CrimeTable.Cols.DATE + ", " + CrimeDbSchema.CrimeTable.Cols.SOLVED + ")");
+                CrimeDbSchema.CrimeTable.Cols.DATE + ", " + CrimeDbSchema.CrimeTable.Cols.SOLVED + CrimeDbSchema.CrimeTable.Cols.SUSPECT + ")");
     }
 
     @Override
