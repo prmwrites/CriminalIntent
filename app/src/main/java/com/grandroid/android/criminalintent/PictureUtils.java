749ee225458133e99aa0969b940148c3fa1dab58
Patrick Morris <prmwrites@gmail.com>
2016-07-04 15:15:10 -0400

    Created PictureUtils with a scaledBitmap converter method

---
+++ app/src/main/java/com/grandroid/android/criminalintent/PictureUtils.java
@@ -0,0 +1,37 @@
+package com.grandroid.android.criminalintent;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+/**
+ * Created by Patrick on 7/4/2016.
+ */
+public class PictureUtils {
+    public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight) {
+        // Read in the dimensions of the image on disk
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+
+        //Figure out how much to scale down by
+        int inSampleSize = 1;
+        if (srcHeight > destHeight || srcWidth > destWidth) {
+            if (srcWidth > srcHeight) {
+                inSampleSize = Math.round(srcHeight / destHeight);
+            } else {
+                inSampleSize = Math.round(srcWidth / destWidth);
+            }
+        }
+
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+
+        // Read in and create final BitMap
+        return BitmapFactory.decodeFile(path, options);
+
+
+    }
+}
